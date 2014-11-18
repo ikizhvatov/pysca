0e8e064ed0a163bd9d81816aa5404dbdf4503824
Ilya Kizhvatov <kizhvatov@riscure.com>
2014-11-18 14:22:09 +0100

    initial refactoring

--- lracpa_swaes.py
+++ lracpa_swaes.py
@@ -174,6 +174,7 @@ def lraAES(data, traces):
 # normalize the matrix of distinguisher traces according to ASIACRYPT'13 proposal
 def normalizeR2Traces(R2):
     R2norm = np.empty(R2.shape)
+    traceLength = R2.shape[1]
     for i in range(0,traceLength): # TODO should be possible to do it in one line without a loop
         R2norm[:,i] = (R2[:,i] - np.mean(R2[:,i])) / np.var(R2[:,i])
     return R2norm
@@ -181,8 +182,10 @@ def normalizeR2Traces(R2):
 ##############################################################################
 ### A. CPA attack stuff
 
-# Hamming weight leakage model
-byteHammingWeight = np.load('data/bytehammingweight.npy') # HW table
+# preload the precomputed lookup tables
+byteHammingWeight = np.load('data/bytehammingweight.npy') # HW of a byte
+sbox = np.load('data/aessbox.npy') # AES S-box
+
 def leakageModel_HW(x):
     return byteHammingWeight[x]
 
@@ -219,110 +222,201 @@ def cpaAES(data, traces):
 
     return CorrTraces
 
+
+##############################################################################
+### Different test flows (TODO: move all below this line to another file,
+###  leaving this file a library only, with a couple of tests)
+
+def testComparePerformance():
+
+    ### 0. Prerequisites
+
+    # the correct key byte (for later metrics)
+    correctKey = np.array([0x2B,0x7E,0x15,0x16,0x28,0xAE,0xD2,0xA6,0xAB,0xF7,0x15,0x88,0x09,0xCF,0x4F,0x3C])
+
+    ### 1. Load samples and data
+
+    # parameters
+    SboxNum = 0
+
+    # readout
+    npzfile = np.load('traces/swaes_atmega_powertraces.npz')
+    data = npzfile['data'][:,SboxNum]      # selecting only the required byte
+    traces = npzfile['traces'][:,900:1100] # selecting the sub-range of samples
+
+    # output traceset parameters
+    (numTraces, traceLength) = traces.shape
+    print "Traceset parameters"
+    print "Number of traces:", numTraces
+    print "Trace length:", traceLength
+
+    ### 2. LRA and CPA with a fixed number of traces
+
+    N = 200 # number of traces
+
+    print "---\nAttacks with %d traces" % N
+    print "Running CPA...",
+    t0 = time.clock()
+    CorrTraces = cpaAES(data[0:N], traces[0:N])
+    t1 = time.clock()
+    print "done in %f s" % (t1 - t0)
+    print "Running LRA...",
+    t0 = time.clock()
+    R2 = lraAES(data[0:N], traces[0:N])
+    t1 = time.clock()
+    print "done in %f s" % (t1 - t0)
+    print "Normalizing LRA results...",
+    R2norm = normalizeR2Traces(R2)
+    print "done"
+
+    print "---\nAttacks with %d traces and conditional averaging" % N
+    print "Performing conditional trace averaging...",
+    t0 = time.clock()
+    (avdata, avtraces) = conditionalAveragingAESSbox(data[0:N], traces[0:N])
+    t1 = time.clock()
+    print "done in %f s" % (t1 - t0)
+    print "Running CPA on averaged traces...",
+    t0 = time.clock()
+    CorrTracesAv = cpaAES(avdata, avtraces)
+    t1 = time.clock()
+    print "done in %f s" % (t1 - t0)
+    print "Running LRA on averaged traces...",
+    t0 = time.clock()
+    R2Av = lraAES(avdata, avtraces)
+    t1 = time.clock()
+    print "done in %f s" % (t1 - t0)
+    print "Normalizing LRA results...",
+    R2AvNorm = normalizeR2Traces(R2Av)
+    print "done"
+
+    ### 3.  visualize the result, highlighting the correct trace
+
+    print "---\nPlotting..."
+    fig, ax = plt.subplots(3,2,sharex=True, squeeze=True)
+
+    WrongKeyRange = range(0, correctKey[SboxNum]) + range(correctKey[SboxNum] + 1, 256)
+
+    ax[0][0].plot(CorrTraces[WrongKeyRange, :].T, color = 'grey')
+    ax[0][0].plot(CorrTraces[correctKey[SboxNum], :], 'r')
+
+    ax[1][0].plot(R2[WrongKeyRange, :].T, color = 'grey')
+    ax[1][0].plot(R2[correctKey[SboxNum], :], 'r')
+
+    ax[2][0].plot(R2norm[WrongKeyRange, :].T, color = 'grey')
+    ax[2][0].plot(R2norm[correctKey[SboxNum], :], 'r')
+
+    ax[0][1].plot(CorrTracesAv[WrongKeyRange, :].T, color = 'grey')
+    ax[0][1].plot(CorrTracesAv[correctKey[SboxNum], :], 'r')
+
+    ax[1][1].plot(R2Av[WrongKeyRange, :].T, color = 'grey')
+    ax[1][1].plot(R2Av[correctKey[SboxNum], :], 'r')
+
+    ax[2][1].plot(R2AvNorm[WrongKeyRange, :].T, color = 'grey')
+    ax[2][1].plot(R2AvNorm[correctKey[SboxNum], :], 'r')
+
+    # same vertical scales for correlation and R2
+    ax[0][0].set_ylim(ax[0][1].get_ylim())
+    ax[1][0].set_ylim(ax[1][1].get_ylim())
+
+    fig.suptitle("CPA and LRA on %d traces" % N)
+    ax[0][0].set_title('Without cond. averaging')
+    ax[0][1].set_title('With cond. averaging')
+    ax[0][0].set_ylabel('Correlation')
+    ax[1][0].set_ylabel('R2')
+    ax[2][0].set_ylabel('Normalized R2')
+    ax[2][0].set_xlabel('Time sample')
+    ax[2][1].set_xlabel('Time sample')
+
+    plt.show()
+
+def testSeriosAttack():
+
+    ### 0. Prerequisites
+
+    # the correct key byte (for later metrics)
+    correctKey = np.array([0x2B,0x7E,0x15,0x16,0x28,0xAE,0xD2,0xA6,0xAB,0xF7,0x15,0x88,0x09,0xCF,0x4F,0x3C])
+
+    ### 1. Load samples and data
+
+    # parameters
+    SboxNum = 0
+
+    # readout
+    npzfile = np.load('traces/swaes_atmega_powertraces.npz')
+    data = npzfile['data'][:,SboxNum]      # selecting only the required byte
+    traces = npzfile['traces'][:,900:1100] # selecting the sub-range of samples
+
+    # output traceset parameters
+    (numTraces, traceLength) = traces.shape
+    print "Traceset parameters"
+    print "Number of traces:", numTraces
+    print "Trace length:", traceLength
+
+    ### 2. LRA and CPA with a fixed number of traces
+
+    N = 200 # number of traces
+
+    print "---\nAttacks with %d traces and conditional averaging" % N
+    print "Performing conditional trace averaging...",
+    t0 = time.clock()
+    (avdata, avtraces) = conditionalAveragingAESSbox(data[0:N], traces[0:N])
+    t1 = time.clock()
+    print "done in %f s" % (t1 - t0)
+    print "Running CPA on averaged traces...",
+    t0 = time.clock()
+    CorrTracesAv = cpaAES(avdata, avtraces)
+    t1 = time.clock()
+    print "done in %f s" % (t1 - t0)
+    print "Running LRA on averaged traces...",
+    t0 = time.clock()
+    R2Av = lraAES(avdata, avtraces)
+    t1 = time.clock()
+    print "done in %f s" % (t1 - t0)
+    print "Normalizing LRA results...",
+    R2AvNorm = normalizeR2Traces(R2Av)
+    print "done"
+
+    ### 3.  visualize the result, highlighting the correct trace
+
+    print "---\nPlotting..."
+    fig, ax = plt.subplots(3,1,sharex=True, squeeze=True)
+
+    WrongKeyRange = range(0, correctKey[SboxNum]) + range(correctKey[SboxNum] + 1, 256)
+
+    ax[0].plot(CorrTracesAv[WrongKeyRange, :].T, color = 'grey')
+    ax[0].plot(CorrTracesAv[correctKey[SboxNum], :], 'r')
+
+    ax[1].plot(R2Av[WrongKeyRange, :].T, color = 'grey')
+    ax[1].plot(R2Av[correctKey[SboxNum], :], 'r')
+
+    ax[2].plot(R2AvNorm[WrongKeyRange, :].T, color = 'grey')
+    ax[2].plot(R2AvNorm[correctKey[SboxNum], :], 'r')
+
+    # same vertical scales for correlation and R2
+    ax[0].set_ylim(ax[0].get_ylim())
+    ax[1].set_ylim(ax[1].get_ylim())
+
+    fig.suptitle("CPA and LRA on %d traces" % N)
+    ax[0].set_title('With cond. averaging')
+    ax[0].set_ylabel('Correlation')
+    ax[1].set_ylabel('R2')
+    ax[2].set_ylabel('Normalized R2')
+    ax[2].set_xlabel('Time sample')
+
+    plt.show()
+
  
 ##############################################################################
 ### The main execution body
 
-### 0. Prerequisites
+if __name__ == '__main__':
 
-# the correct key byte (for later metrics)
-correctKey = np.array([0x2B,0x7E,0x15,0x16,0x28,0xAE,0xD2,0xA6,0xAB,0xF7,0x15,0x88,0x09,0xCF,0x4F,0x3C])
+    # TODO: common file-reading code should be here
 
-# the precomputed lookup tables
-sbox = np.load('data/aessbox.npy')
-byteHammingWeight = np.load('data/bytehammingweight.npy')
+    # run different scenarious defined as functions above
 
-### 1. Load samples and data
+    #testComparePerformance()
 
-# parameters
-SboxNum = 0
+    testSeriosAttack()
 
-# readout
-npzfile = np.load('traces/swaes_atmega_powertraces.npz')
-data = npzfile['data'][:,SboxNum]      # selecting only the required byte
-traces = npzfile['traces'][:,900:1100] # selecting the sub-range of samples
 
-# output traceset parameters
-(numTraces, traceLength) = traces.shape
-print "Traceset parameters"
-print "Number of traces:", numTraces
-print "Trace length:", traceLength
-
-### 2. LRA and CPA with a fixed number of traces
-
-N = 200 # number of traces
-
-print "---\nAttacks with %d traces" % N
-print "Running CPA...",
-t0 = time.clock()
-CorrTraces = cpaAES(data[0:N], traces[0:N])
-t1 = time.clock()
-print "done in %f s" % (t1 - t0)
-print "Running LRA...",
-t0 = time.clock()
-R2 = lraAES(data[0:N], traces[0:N])
-t1 = time.clock()
-print "done in %f s" % (t1 - t0)
-print "Normalizing LRA results...",
-R2norm = normalizeR2Traces(R2)
-print "done"
-
-print "---\nAttacks with %d traces and conditional averaging" % N
-print "Performing conditional trace averaging...",
-t0 = time.clock()
-(avdata, avtraces) = conditionalAveragingAESSbox(data[0:N], traces[0:N])
-t1 = time.clock()
-print "done in %f s" % (t1 - t0)
-print "Running CPA on averaged traces...",
-t0 = time.clock()
-CorrTracesAv = cpaAES(avdata, avtraces)
-t1 = time.clock()
-print "done in %f s" % (t1 - t0)
-print "Running LRA on averaged traces...",
-t0 = time.clock()
-R2Av = lraAES(avdata, avtraces)
-t1 = time.clock()
-print "done in %f s" % (t1 - t0)
-print "Normalizing LRA results...",
-R2AvNorm = normalizeR2Traces(R2Av)
-print "done"
-
-### 3.  visualize the result, highlighting the correct trace
-
-print "---\nPlotting..."
-fig, ax = plt.subplots(3,2,sharex=True, squeeze=True)
-
-WrongKeyRange = range(0, correctKey[SboxNum]) + range(correctKey[SboxNum] + 1, 256)
-
-ax[0][0].plot(CorrTraces[WrongKeyRange, :].T, color = 'grey')
-ax[0][0].plot(CorrTraces[correctKey[SboxNum], :], 'r')
-
-ax[1][0].plot(R2[WrongKeyRange, :].T, color = 'grey')
-ax[1][0].plot(R2[correctKey[SboxNum], :], 'r')
-
-ax[2][0].plot(R2norm[WrongKeyRange, :].T, color = 'grey')
-ax[2][0].plot(R2norm[correctKey[SboxNum], :], 'r')
-
-ax[0][1].plot(CorrTracesAv[WrongKeyRange, :].T, color = 'grey')
-ax[0][1].plot(CorrTracesAv[correctKey[SboxNum], :], 'r')
-
-ax[1][1].plot(R2Av[WrongKeyRange, :].T, color = 'grey')
-ax[1][1].plot(R2Av[correctKey[SboxNum], :], 'r')
-
-ax[2][1].plot(R2AvNorm[WrongKeyRange, :].T, color = 'grey')
-ax[2][1].plot(R2AvNorm[correctKey[SboxNum], :], 'r')
-
-# same vertical scales for correlation and R2
-ax[0][0].set_ylim(ax[0][1].get_ylim())
-ax[1][0].set_ylim(ax[1][1].get_ylim())
-
-fig.suptitle("CPA and LRA on %d traces" % N)
-ax[0][0].set_title('Without cond. averaging')
-ax[0][1].set_title('With cond. averaging')
-ax[0][0].set_ylabel('Correlation')
-ax[1][0].set_ylabel('R2')
-ax[2][0].set_ylabel('Normalized R2')
-ax[2][0].set_xlabel('Time sample')
-ax[2][1].set_xlabel('Time sample')
-
-plt.show()
