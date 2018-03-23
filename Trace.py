cbd8cf96f19b3c83c428f992cfcb4b44ddaf7f6a
Ilya Kizhvatov <ilya.kizhvatov@gmail.com>
2018-03-23 13:46:35 +0100

    write support in trace interface

--- Trace.py
+++ Trace.py
@@ -10,6 +10,9 @@ Versions of this file:
 v0.6 28-05-2017 (Pol)
     * performance greatly improved by using np.fromfile
 
+v0.55 05-12-2014 (yet unidentified author at Riscure)
+    * Added writing trace fileSize
+
 v0.5 12-11-2013 (Ilya)
     * Fixed the format typo; a bit of cleanup
 
@@ -23,12 +26,9 @@ v0.2 5-3-2013 (Erik)
     * Fixed __iter__ method and removed the no longer necesarry getTraces() method
     * Moved header constants inside TraceSet class
     * Removed debug code
-
-Todo:
-    * Save tracesets back to file
 '''
 
-from __future__ import print_function
+from __future__     import print_function
 
 import sys
 import struct
@@ -76,6 +76,7 @@ class TraceSet():
         self._titleSpace = 0
         self._dataSpace = 0
         self._yscale = 1
+        self._fileName = None
 
         #properties
         self._sampleSpace = None
@@ -98,7 +99,60 @@ class TraceSet():
         return struct.unpack("I",self._handle.read(4))[0];
 
     def _readFloat32(self):
-        return struct.unpack("f",self._handle.read(4))[0]
+        return struct.unpack("f",self._handle.read(4))[0];
+
+    def _writeUINT8(self, val):
+        #print "UINT8: %02x"%(val&0xFF)
+        return self._handle.write(struct.pack("B",(val&0xFF)));
+
+    def _writeUINT16(self, val):
+        #print "UINT16: %04x"%(val&0xFFFF)
+        return self._handle.write(struct.pack("H",(val&0xFFFF)));
+
+    def _writeUINT32(self, val):
+        #print "UINT32: %08x"%(val&0xFFFFFFFF)
+        return self._handle.write(struct.pack("I",(val&0xFFFFFFFF)));
+
+    def _writeTitleSpace(self, val):
+        self._writeUINT8(self.TitleSpace)
+        self._writeUINT8(1)
+        self._writeUINT8(val)
+        self._titleSpace = val
+
+    def _writeNumberOfTraces(self, val):
+        self._writeUINT8(self.NumberOfTraces)
+        self._writeUINT8(4)
+        self._writeUINT32(val)
+        self._numberOfTraces = val
+
+    def _updateNumberOfTraces(self, val):
+        self.findTag(self.NumberOfTraces)
+        self._writeUINT8(self.NumberOfTraces)
+        self._writeUINT8(4)
+        self._writeUINT32(val)
+        self._numberOfTraces = val
+
+    def _writeDataSpace(self, val):
+        self._writeUINT8(self.DataSpace)
+        self._writeUINT8(2)
+        self._writeUINT16(val)
+        self._dataSpace = val
+
+    def _writeNumberOfSamplesPerTrace(self, val):
+        self._writeUINT8(self.NumberOfSamplesPerTrace)
+        self._writeUINT8(4)
+        self._writeUINT32(val)
+        self._numberOfSamplesPerTrace = val
+
+    def _writeSampleCoding(self, val):
+        self._writeUINT8(self.SampleCoding)
+        self._writeUINT8(1)
+        self._writeUINT8(val)
+        self._sampleCoding = val
+
+    def _writeTraceBlock(self):
+        self._writeUINT8(self.TraceBlock)
+        self._writeUINT8(0)
 
     def open(self, fileName):
         self._handle = open(fileName,'rb')
@@ -164,12 +218,86 @@ class TraceSet():
 
             offset = offset + 2 + addLen + length
 
+    def findTag(self,searchTag):
+        if (self._handle == None):
+            return 0
+        f = self._handle
+        f.seek(0,2)
+        fileSize = f.tell()
+        f.seek(0)
+        offset = 0
+
+        while (offset < fileSize - self._traceBlockSpace):
+            tag = ord(f.read(1))
+            length = ord(f.read(1))
+            addLen = 0
+
+            if ((length & 0x80) != 0): #length is encoded in more then 1 byte
+                addLen = length & 0x7F #how many byte the length is actually encoded in.
+                length = 0
+                for i in range(addLen):
+                    length = length + (ord(f.read(1)) << (i * 8))
+            if (tag == searchTag):
+                f.seek(offset)
+                return 1
+            f.read(length)
+            offset = offset + 2 + addLen + length
+        return 0
+
+    def new(self, fileName, titleSpace, sampleCoding, dataSpace, numberOfSamples):
+        self._fileName = fileName
+        self._handle = open(fileName,'wb')
+        self._handle.close()
+        self._handle = open(fileName,'r+b')
+
+        self._writeTitleSpace(titleSpace)
+        self._writeSampleCoding(sampleCoding)
+        if self._sampleCoding == self.CodingFloat: #float
+            self._sampleCodingByteSize = 4
+            self._npSampleCoding = "float32"
+        else:
+            self._npSampleCoding = "int" + str(self._sampleCoding * 8)
+            self._sampleCodingByteSize = self._sampleCoding
+        self._writeDataSpace(dataSpace)
+        self._writeNumberOfSamplesPerTrace(numberOfSamples)
+        self._writeNumberOfTraces(0)
+        self._sampleSpace = self._numberOfSamplesPerTrace * self._sampleCodingByteSize
+        self._traceSpace = self._sampleSpace + self._dataSpace + self._titleSpace
+        self._traceBlockSpace = self._numberOfTraces * self._traceSpace
+        self._writeTraceBlock()
+        self._traceBlockOffset = self._handle.tell() #get current pos
+
     def getTrace(self, traceIndex):
+        if (self._handle == None):
+            return None
         f = self._handle
         f.seek(self._traceBlockOffset + traceIndex * self._traceSpace)
-
+        
         title = f.read(self._titleSpace)
         data = np.fromfile(f, dtype='uint8', count=self._dataSpace)
         samples = np.fromfile(f, dtype=self._npSampleCoding, count=self._numberOfSamplesPerTrace)
 
         return Trace(title, data, samples)
+
+    def addTrace(self, trace):
+        if (self._handle == None):
+            return
+        f = self._handle
+        f.seek(0,2)
+
+        # we only accept exact same size inputs
+        if (len(trace._title) != self._titleSpace):
+            raise ValueError("Wrong title size! %08x/%08x"%(len(trace._title),self._titleSpace))
+        f.write(trace._title)
+        if (len(trace._data) != self._dataSpace):
+            raise ValueError("Wrong data size!")
+        trace._data.tofile(f)
+        trace._samples.tofile(f)
+        self._numberOfTraces+=1
+        # update the trace count
+        self._updateNumberOfTraces(self._numberOfTraces)
+        # go to the end again
+        f.seek(0,2)
+
+    def close(self):
+        self._handle.close()
